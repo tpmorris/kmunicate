6fbb1ea7a78d52c31f76919251ab3fe310a2a6c4
Alessandro Gasparini <ellessenne@gmail.com>
2020-07-10 11:13:44 +0200

    Plot is better now

--- R/alessandro-gasparini.R
+++ R/alessandro-gasparini.R
@@ -16,19 +16,28 @@ dplyr::bind_rows(KM_data, KM_data_zero)
 KM_data <- rbind.data.frame(KM_data, KM_data_zero)
 
 # Define time scale for the x-axis
+# This is used to calculate the tables at the bottom, and values will be aligned to the ticks on the x-axis
+# Modify time_scale and everything should be updated automatically...
 time_scale <- seq(0, max(brcancer$rectime), by = 365)
+
+# Better font for plots:
+.ff <- "Roboto Condensed"
 
 # Begin building the plot
 # ...using ggplot2 of course
 library(ggplot2)
 library(pammtools) # for geom_stepribbon
+library(ggthemes)
 plot <- ggplot(KM_data, aes(x = time, y = surv)) +
   pammtools::geom_stepribbon(aes(ymin = lower, ymax = upper, fill = strata), alpha = 0.2) +
   geom_step(aes(color = strata, linetype = strata)) +
   scale_x_continuous(breaks = time_scale) +
+  ggthemes::scale_color_colorblind() +
+  ggthemes::scale_fill_colorblind() +
   coord_cartesian(ylim = c(0, 1), xlim = range(time_scale)) +
   labs(color = "", fill = "", linetype = "", x = "Time in days", y = "Estimated survival") +
-  theme(legend.position = c(1, 1), legend.justification = c(1, 1), legend.background = element_blank())
+  theme_minimal(base_family = .ff) +
+  theme(legend.position = c(1, 1), legend.justification = c(1, 1), legend.background = element_blank(), panel.grid.minor = element_blank())
 
 # Ticks on the x-axis are every 365 days, so we need to create a summary dataset to create the table with data
 KM_data$table_group <- findInterval(x = KM_data$time, vec = time_scale, left.open = TRUE)
@@ -52,12 +61,12 @@ table_data <- pivot_longer(data = table_data, cols = c("events", "censor", "at_r
 tds <- split(table_data, f = table_data$strata)
 tds <- lapply(seq_along(tds), function(i) {
   ggplot(tds[[i]], aes(x = time_scale, y = name, label = value)) +
-    geom_text() +
+    geom_text(aes(family = .ff)) +
     scale_x_continuous(breaks = time_scale) +
     scale_y_discrete(limits = rev(levels(tds[[i]]$name))) +
     coord_cartesian(xlim = range(time_scale)) +
-    theme_void() +
-    theme(axis.text.y = element_text(face = "italic")) +
+    theme_void(base_family = .ff) +
+    theme(axis.text.y = element_text(face = "italic"), plot.margin = margin(0, 0, 0, 0, "cm")) +
     labs(title = names(tds)[i])
 })
 
@@ -67,4 +76,4 @@ KM_plot <- plot_grid(plotlist = c(list(plot), tds), align = "hv", axis = "tlbr",
 # You might have to adjust the arguments of plot_grid to make it look the way you want
 KM_plot
 # Export plot as a .png
-ggsave(KM_plot, filename = "R/alessandro-gasparini-1.png", dpi = 600, height = 7, width = 7 / sqrt(2))
+ggsave(KM_plot, filename = "R/alessandro-gasparini-2.png", dpi = 600, height = 7, width = 7 / sqrt(2))
